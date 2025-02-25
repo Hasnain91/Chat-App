f322dba2f7076aab7afd561a6a68f91d7536ea6f
Hasnain Iftikhar <hasnainrao@yahoo.com>
2025-02-25 15:12:33 +0500

    user signup controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,58 @@
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/userModal");
+const generateToken = require("../lib/utils");
+
 // User Signup
-const signup = (req, res) => {
-  res.send("Sign Up Route is now working");
+const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    // 0. Required Fields Validation
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    // 1. Password Validation
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 characters." });
+    }
+
+    // 2. Check if user already exists
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "Email already exists." });
+
+    //3.  Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // 4. Create new user
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      // generate token here
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid User Data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller: ", error);
+    res.status(500).json({ message: "Internal Server Error." });
+  }
 };
 
 // User Login
